cd14b28deb7da11e72d2be56c71b7d570b0694ff
jHuang <379751452@qq.com>
2023-11-28 11:16:29 +0800

    update docs deploy

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import VueJsx from '@vitejs/plugin-vue-jsx'
+import minimist from 'minimist'
+
+const argv = minimist(process.argv.slice(2))
+const build = argv.build || false
 
 export default defineConfig({
+  base: build ? '/yyg-demo-ui/' : '/',
   plugins: [
     VueJsx()
   ],
