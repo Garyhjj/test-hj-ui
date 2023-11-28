dba9982baf1017acfb45a27f91701b475483e425
jHuang <379751452@qq.com>
2023-11-28 11:59:39 +0800

    fix base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,12 @@ import { DefaultTheme, defineConfig } from 'vitepress'
 import { componentPreview, containerPreview } from '@vitepress-demo-preview/plugin'
 import { components } from '../components'
 
+import minimist from 'minimist'
+
+const argv = minimist(process.argv.slice(2))
+const build = argv.build || false
+console.log(build)
+
 const nav: DefaultTheme.NavItem[] = [
   { text: '指南', link: '/guide/' },
   { text: '组件', link: '/components/foo' }
@@ -25,14 +31,14 @@ const sidebar: DefaultTheme.Sidebar = {
 }
 
 export default defineConfig({
-  title: 'yyg-admin-ui',
-  description: 'YYG Vue3企业级中后台组件库',
+  title: 'Hj-ui',
+  description: 'Hj Vue3企业级中后台组件库',
   lang: 'cn-ZH',
-  base: '/',
+  base: build ? '/test-hj-ui' : '/',
   lastUpdated: true,
   themeConfig: {
     logo: '/logo.png',
-    siteTitle: 'yyg-admin-ui',
+    siteTitle: 'hj-ui',
     outline: 3,
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import VueJsx from '@vitejs/plugin-vue-jsx'
-import minimist from 'minimist'
-
-const argv = minimist(process.argv.slice(2))
-const build = argv.build || false
 
 export default defineConfig({
-  base: build ? '/test-hj-ui/' : '/',
   plugins: [
     VueJsx()
   ],
